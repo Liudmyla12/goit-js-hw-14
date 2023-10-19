b4da557511a611cb70ee5fefa49a46971288f882
Liudmyla12 <133156907+Liudmyla12@users.noreply.github.com>
2023-10-19 21:02:00 +0200

    git push -u origin master

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector("#categories");
+const items = categoriesList.querySelectorAll(".item");
+
+console.log(`Total categories: ${items.length}`);
+
+items.forEach((item) => {
+  const title = item.firstElementChild;
+  const subCategories = item.lastElementChild.children;
+
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Number of items: ${subCategories.length}`);
+});
